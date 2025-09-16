7507cde581fca374873b910db8f232938377e33c
Victor Paris Lemperrière <vic.pl930@gmail.com>
2025-09-16 08:33:54 +0200

    First commit

---
+++ exercices.js
@@ -0,0 +1,22 @@
+
+/*
+// Exercice 1
+let degCelsius = 22;
+
+function convertToFahrenheit(degCelsius) {
+    return degCelsius * (9/5) + 32;
+}
+
+console.log("degrès celsius: " + degCelsius);
+console.log("degrès fahenheit: " + convertToFahrenheit(degCelsius));
+*/
+
+
+// Exercice 2
+let rectWidth = 10, rectHeight = 8;
+
+function calculAireRect(width, height) {
+    return width * height;
+}
+
+console.log("Aire : " + calculAireRect(rectWidth, rectHeight));
